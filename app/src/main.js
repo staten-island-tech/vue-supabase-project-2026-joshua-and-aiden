7910d373513f3eab8ed1bcc68771d7b5f36befae
Aiden Hu <125307388+TheCoder122@users.noreply.github.com>
2026-06-11 13:35:15 -0400

    Update main.js

--- app/src/main.js
+++ app/src/main.js
@@ -3,10 +3,11 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import { useAuthStore } from './stores/auth'
 
 const app = createApp(App)
-
-app.use(createPinia())
+const pinia = createPinia()
+app.use(pinia)
 app.use(router)
-
+const auth = useAuthStore()
 app.mount('#app')
